ad12d63139eb79bf895d78bb565ebd7a84c08fca
Wilton Baltazar <wiltonbaltazar@gmail.com>
2026-03-18 14:59:31 +0200

    fix: login for admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->redirectUsersTo('/admin');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureUserRole::class,
         ]);
